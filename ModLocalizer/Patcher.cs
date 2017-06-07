94ba13f1a361efa200a7d310274e9c61c6d4bc96
ZIT WANG <mymisting@gmail.com>
2017-06-07 15:46:21 +0800

    Finish npc/buff patch

--- ModLocalizer/Patcher.cs
+++ ModLocalizer/Patcher.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using dnlib.DotNet;
@@ -40,6 +41,8 @@ namespace ModLocalizer
 
 			ApplyItems();
 			ApplyNpcs();
+			ApplyBuffs();
+			ApplyMiscs();
 
 			Save();
 		}
@@ -96,7 +99,6 @@ namespace ModLocalizer
 					return;
 
 				var fullName = string.Concat(translation.Namespace, ".", translation.TypeName);
-
 				var type = emitter.Module.Find(fullName, true);
 
 				if (type == null)
@@ -159,7 +161,170 @@ namespace ModLocalizer
 
 		private void ApplyNpcs()
 		{
+			var texts = LoadTranslations<NpcTranslation>("Npcs");
 
+			foreach (var text in texts)
+			{
+				ApplyNpcsInternal(text, _emitter);
+				ApplyNpcsInternal(text, _monoEmitter);
+			}
+
+			void ApplyNpcsInternal(NpcTranslation translation, TranslationEmitter emitter)
+			{
+				if (translation == null || emitter == null)
+					return;
+
+				var fullName = string.Concat(translation.Namespace, ".", translation.TypeName);
+				var type = emitter.Module.Find(fullName, true);
+
+				if (type == null)
+					return;
+
+				var method = type.FindMethod("SetStaticDefaults", MethodSig.CreateInstance(_module.CorLibTypes.Void));
+				if (!string.IsNullOrEmpty(translation.Name))
+					emitter.Emit(method, "DisplayName", translation.Name);
+
+				method = type.FindMethod("GetChat");
+				if (method?.HasBody == true)
+				{
+					var inst = method.Body.Instructions;
+
+					var listindex = 0;
+
+					for (var index = 0; index < inst.Count; index++)
+					{
+						var ins = inst[index];
+
+						if (ins.OpCode != OpCodes.Ldstr)
+							continue;
+
+						ins = inst[++index];
+
+						if ((ins.OpCode.Equals(OpCodes.Call) || ins.OpCode.Equals(OpCodes.Callvirt))
+						    && ins.Operand is IMethodDefOrRef m)
+							if (!m.Name.ToString().Equals("Concat") || m.MethodSig.Params.Count == 1)
+								continue;
+
+						inst[index - 1].Operand = translation.ChatTexts[listindex++];
+					}
+				}
+
+				method = type.FindMethod("SetChatButtons");
+				if (method?.HasBody == true)
+				{
+					var inst = method.Body.Instructions;
+
+					for (var index = 0; index < inst.Count; index++)
+					{
+						var ins = inst[index];
+
+						if (ins.OpCode != OpCodes.Ldstr)
+							continue;
+
+						ins = inst[index - 1];
+
+						if (ins.OpCode.Equals(OpCodes.Ldarg_1))
+							inst[index].Operand = translation.ShopButton1;
+						else if (ins.OpCode.Equals(OpCodes.Ldarg_2))
+							inst[index].Operand = translation.ShopButton2;
+					}
+				}
+			}
+		}
+
+		private void ApplyBuffs()
+		{
+			var texts = LoadTranslations<BuffTranslation>("Buffs");
+
+			foreach (var text in texts)
+			{
+				ApplyBuffsInternal(text, _emitter);
+				ApplyBuffsInternal(text, _monoEmitter);
+			}
+
+			void ApplyBuffsInternal(BuffTranslation translation, TranslationEmitter emitter)
+			{
+				if (translation == null || emitter == null)
+					return;
+
+				var fullName = string.Concat(translation.Namespace, ".", translation.TypeName);
+				var type = emitter.Module.Find(fullName, true);
+
+				if (type == null)
+					return;
+
+				var method = type.FindMethod("SetDefaults", MethodSig.CreateInstance(_module.CorLibTypes.Void));
+				if (!string.IsNullOrEmpty(translation.Name))
+					emitter.Emit(method, "DisplayName", translation.Name);
+				if (!string.IsNullOrEmpty(translation.Tip))
+					emitter.Emit(method, "Description", translation.Tip);
+			}
+		}
+
+		private void ApplyMiscs()
+		{
+			var texts = LoadTranslations<MiscTranslation>("Miscs");
+
+			foreach (var text in texts)
+			{
+				ApplyMiscsInternal(text, _emitter);
+				ApplyMiscsInternal(text, _monoEmitter);
+			}
+
+			void ApplyMiscsInternal(MiscTranslation translation, TranslationEmitter emitter)
+			{
+				if (translation == null || emitter == null)
+					return;
+
+				var fullName = string.Concat(translation.Namespace, ".", translation.TypeName);
+				var type = emitter.Module.Find(fullName, true);
+
+				var method = type?.FindMethod(translation.Method);
+
+				if (method?.HasBody != true)
+					return;
+
+				var inst = method.Body.Instructions;
+				var listIndex = 0;
+
+				for (var index = 0; index < inst.Count; index++)
+				{
+					var ins = inst[index];
+
+					if (!ins.OpCode.Equals(OpCodes.Call) || !(ins.Operand is IMethodDefOrRef m) ||
+					    !m.Name.ToString().Equals("NewText", StringComparison.Ordinal))
+						continue;
+
+					if ((ins = inst[index - 5]).OpCode.Equals(OpCodes.Ldstr))
+					{
+						ins.Operand = translation.Contents[listIndex++];
+					}
+					else if (ins.OpCode.Equals(OpCodes.Call) &&
+					         ins.Operand is IMethodDefOrRef n &&
+					         n.Name.ToString().Equals("Concat", StringComparison.Ordinal))
+					{
+						var index2 = index;
+						var count = 0;
+						var total = n.MethodSig.Params.Count;
+						var list = translation.Contents.GetRange(listIndex, translation.Contents.Count - listIndex).AsEnumerable().Reverse().ToList();
+						var currentIndex = 0;
+						while (--index2 > 0 && count < total)
+						{
+							ins = inst[index2];
+							if (ins.OpCode.Equals(OpCodes.Ldelem_Ref)) // for array
+							{
+								count++;
+							}
+							else if (ins.OpCode.Equals(OpCodes.Ldstr))
+							{
+								count++;
+								listIndex++;
+								ins.Operand = list[currentIndex++];
+							}
+						}
+					}
+				}
+			}
 		}
 
 		private void Save()
