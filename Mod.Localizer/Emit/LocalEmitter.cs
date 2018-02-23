8d9d0e6e9eab711c6f1b4f751627c0b1b6277663
Ziteng Wang <mymisting@gmail.com>
2018-02-22 16:32:20 -0800

    Add local emitter base

---
+++ Mod.Localizer/Emit/LocalEmitter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using Mod.Localizer.Emit.Provider;
+
+namespace Mod.Localizer.Emit
+{
+    public sealed class LocalEmitter : Emitter
+    {
+        public LocalEmitter(MethodDef method, ITranslationBaseProvider provider) : base(method, provider)
+        {
+        }
+
+        public override void Emit(Instruction target, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsTarget(Instruction instruction)
+        {
+            return false;
+        }
+    }
+}
