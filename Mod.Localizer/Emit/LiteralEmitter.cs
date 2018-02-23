6909f63db4a1d303fa7918cc954e299a9b91c2f8
Ziteng Wang <mymisting@gmail.com>
2018-02-22 16:31:34 -0800

    Add literal emitter

--- Mod.Localizer/Emit/LiteralEmitter.cs
+++ Mod.Localizer/Emit/LiteralEmitter.cs
@@ -1,12 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using Mod.Localizer.Emit.Provider;
 
 namespace Mod.Localizer.Emit
 {
-    public sealed class LiteralEmitter
+    public sealed class LiteralEmitter : Emitter
     {
+        public LiteralEmitter(MethodDef method, ITranslationBaseProvider provider) : base(method, provider)
+        {
+        }
+
+        public override void Emit(Instruction target, string value)
+        {
+            // replace the string loaded with a call to `GetTextValue` of translation
+
+            // first replace the string literal to translation key
+            var key = Provider.CreateTranslation(Method.DeclaringType.Name, (string)target.Operand);
+            target.Operand = key;
+
+            // insert the static method call right after the target instruction
+            var index = Method.Body.Instructions.IndexOf(target);
+            if (index == -1) throw new ArgumentOutOfRangeException(nameof(target));
+            Method.Body.Instructions.Insert(index + 1, OpCodes.Call.ToInstruction(Provider.GetTextValueMethod));
+
+            // simplify branches
+            Method.Body.SimplifyBranches();
+            Method.Body.OptimizeBranches();
+        }
+
+        public override bool IsTarget(Instruction instruction)
+        {
+            return instruction.OpCode == OpCodes.Ldstr;
+        }
     }
 }
