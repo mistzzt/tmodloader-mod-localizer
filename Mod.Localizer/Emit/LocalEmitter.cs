74ea0d1c9c4c3e846daab27dc85208a473399ba3
Ziteng Wang <mymisting@gmail.com>
2018-03-04 19:07:24 -0800

    Fix translation set default override

--- Mod.Localizer/Emit/LocalEmitter.cs
+++ Mod.Localizer/Emit/LocalEmitter.cs
@@ -20,8 +20,26 @@ namespace Mod.Localizer.Emit
                 throw new ArgumentOutOfRangeException(nameof(target));
             }
 
-            // insert after the target
-            var index = Method.Body.Instructions.IndexOf(target) + 1;
+            // target is expected to be `stloc.0` after the translation object
+            // has been created. Here, we find the next `ldloc.0`
+            var instructions = Method.Body.Instructions;
+            var startIndex = instructions.IndexOf(target) + 1;
+            var index = startIndex;
+            while (instructions[index++].OpCode != OpCodes.Ldloc_0 && index < instructions.Count)
+            {
+                // empty
+            }
+
+            // if we didn't find the next `ldloc.0`, we use the original `stloc.0`
+            if (index == instructions.Count)
+            {
+                index = startIndex;
+            }
+            else
+            {
+                index += 2;
+            }
+
             Method.Body.Instructions.Insert(index, new[]
             {
                 OpCodes.Ldloc_S.ToInstruction(local),
