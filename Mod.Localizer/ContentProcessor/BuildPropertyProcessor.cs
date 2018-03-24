032091b8ab56187aeee322ad69ad4def3f8400c5
Ziteng Wang <mymisting@gmail.com>
2018-03-23 18:42:27 -0700

    Implement build prop processing

--- Mod.Localizer/ContentProcessor/BuildPropertyProcessor.cs
+++ Mod.Localizer/ContentProcessor/BuildPropertyProcessor.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using dnlib.DotNet;
 using Mod.Localizer.ContentFramework;
+using Mod.Localizer.Extensions;
+using Mod.Localizer.Resources;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using Terraria;
@@ -20,19 +23,32 @@ namespace Mod.Localizer.ContentProcessor
 
         public override IReadOnlyList<Content> DumpContents()
         {
+            var path = this.GetExtraDataPath();
             var json = _helper.Load(ModFile);
 
-            _helper.Write(ModFile, json);
+            File.WriteAllText(path, json);
+            Logger.Info(Strings.BuildPropWrittenToPath, path);
 
             return new List<Content>();
         }
 
         public override void PatchContents(IReadOnlyList<Content> contents)
         {
+            var path = this.GetExtraDataPath();
+            if (!File.Exists(path))
+            {
+                return;
+            }
 
+            using (var sr = new StreamReader(File.OpenRead(path)))
+            {
+                var json = sr.ReadToEnd();
+                
+                _helper.Write(ModFile, json);
+            }
         }
 
-        private class BuildPropHelper
+        private sealed class BuildPropHelper
         {
             public string Load(TmodFileWrapper.ITmodFile modFile)
             {
@@ -54,19 +70,23 @@ namespace Mod.Localizer.ContentProcessor
 
             public void Write(TmodFileWrapper.ITmodFile modFile, string json)
             {
+                // Creates a new blank information object from json data
                 var obj = JsonConvert.DeserializeObject(json, _readBuildFile.DeclaringType, new JsonSerializerSettings
                 {
                     ContractResolver = new StringFieldContractResolver()
                 });
 
+                // Loads the property object within current mod
                 var info = LoadRaw(modFile);
 
+                // Replaces target fields
                 foreach (var field in obj.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance).Where(FieldSelector))
                 {
                     var replace = field.GetValue(obj);
                     field.SetValue(info, replace);
                 }
 
+                // Serializes the property object and store it inside mod file
                 var data = (byte[])_toBytes.Invoke(info, new object[0]);
                 modFile.Files["Info"] = data;
             }
@@ -84,6 +104,9 @@ namespace Mod.Localizer.ContentProcessor
             private readonly MethodInfo _readBuildFile;
             private readonly MethodInfo _toBytes;
 
+            /// <summary>
+            /// Resolver to include all private fields in <see cref="Terraria.ModLoader.BuildProperties"/>.
+            /// </summary>
             private sealed class StringFieldContractResolver : DefaultContractResolver
             {
                 protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
@@ -97,6 +120,9 @@ namespace Mod.Localizer.ContentProcessor
                 }
             }
 
+            /// <summary>
+            /// Selects all fields of <see cref="Terraria.ModLoader.BuildProperties"/> that should be included in serialization.
+            /// </summary>
             private static bool FieldSelector(FieldInfo f) => f.FieldType == typeof(string);
         }
     }
